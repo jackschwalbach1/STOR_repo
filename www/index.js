8825e1c045ef54a606943fb2cc351de6fc1dc68b
jackschwalbach1 <jackschwalbach1@gmail.com>
2024-04-22 03:21:28 -0600

    still not working

--- www/index.js
+++ www/index.js
@@ -5,39 +5,3 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const pgp = require('pg-promise')();
-require('dotenv').config();
-
-const app = express();
-app.use(bodyParser.json());
-
-const dbConfig = {
-  host: 'db',
-  port: 5432,
-  database: process.env.POSTGRES_DB,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-};
-const db = pgp(dbConfig);
-
-db.connect()
-  .then(obj => {
-    console.log('Database connection successful'); // you can view this message in the docker compose logs
-    obj.done(); // success, release the connection;
-  })
-  .catch(error => {
-    console.log('ERROR:', error.message || error);
-  });
-
-
-const message = 'Hey there!';
-app.get('/', (req, res) => {
-  res.send(message);
-});
-
-app.listen(3000, () => {
-  console.log('listening on port 3000');
-});
